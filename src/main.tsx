98aab5854dbd5e070dd4fc698b8a2ecb03cd5df6
Mutammim <58490040+mutammim@users.noreply.github.com>
2022-06-27 20:21:31 +0000

    ✨ Custom message count (and lots of refactoring)

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import "dotenv/config";
 /* -------------------------- Import other imports -------------------------- */
 
 import { readFileSync, writeFileSync } from "fs";
-import { App } from "@slack/bolt";
+import { App, RespondFn } from "@slack/bolt";
 import {
 	Section,
 	Blocks,
@@ -18,6 +18,8 @@ import {
 	Field,
 	Fragment,
 } from "jsx-slack";
+
+import { Match } from "@slack/web-api/dist/response/SearchMessagesResponse";
 
 /* ----------------------- Set up bot and user clients ---------------------- */
 
@@ -42,68 +44,48 @@ const userApp = new App({
 	/*                               React to /bunny                              */
 	/* -------------------------------------------------------------------------- */
 
-	botApp.command("/bunny", async ({ ack, respond }) => {
+	botApp.command("/bunny", async ({ ack, command, respond }) => {
 		await ack();
 
-		let channelCounts = {};
+		let count = 100;
+		let allMessages;
+		let sortedChannels;
+
+		/* -------------------------- Process command text -------------------------- */
+
+		if (command.text && !isNaN(parseInt(command.text))) {
+			count = parseInt(command.text);
+		}
+
+		/* ------------------------ Reject count beyond 5000 ------------------------ */
+
+		if (count > 1000) {
+			respond({
+				text: `Sorry, ${count} is a bit excessive. Try 1000 or lower?`,
+			});
+			return;
+		}
 
 		/* -------------------------------- Get data -------------------------------- */
 
-		let messageData = await userApp.client.search.messages({
-			query: "-is:dm",
-			sort: "timestamp",
-			sort_dir: "desc",
-			count: 100,
-		});
+		allMessages = await getLatestMessages(count, respond);
 
 		/* ------------------------------ Get channels ------------------------------ */
 
-		messageData.messages.matches.map((match) => {
-			/* ---------------------- Return if not public channel ---------------------- */
-
-			if (
-				match.channel?.is_channel !== true ||
-				match.channel?.is_private === true
-			)
-				return;
-
-			/* -------------------------------- Otherwise ------------------------------- */
-
-			let channelID = match.channel?.id as string;
-
-			// If channel doesn't exist yet, add the key
-			if (channelCounts[channelID] === undefined)
-				channelCounts[channelID] = 0;
-
-			// Increment the count
-			channelCounts[channelID] += 1;
-		});
-
-		/* ------------------------------ Sort channels ----------------------------- */
-
-		let sortedChannels = Object.keys(channelCounts).sort(function (a, b) {
-			return channelCounts[b] - channelCounts[a];
-		});
+		sortedChannels = await getSortedChannels(allMessages);
 
 		/* ------------------ Get channel topics for top 5 channels ----------------- */
 
-		let topFiveChannelTopics: string[] = [];
-
-		for (const channel of sortedChannels.slice(0, 5)) {
-			let topic = (
-				await botApp.client.conversations.info({
-					channel,
-				})
-			).channel.topic.value;
-
-			topFiveChannelTopics.push(topic);
-		}
+		let topFiveChannelTopics = await getChannelTopics(
+			sortedChannels.slice(0, 5)
+		);
 
 		/* -------------------------------- Build UI -------------------------------- */
 
 		const topChannelsUI = (
 			<Fragment>
 				<Header>Top channels</Header>
+
 				{sortedChannels.slice(0, 5).map((channel) => {
 					let topicText =
 						topFiveChannelTopics[sortedChannels.indexOf(channel)];
@@ -126,33 +108,35 @@ const userApp = new App({
 			</Fragment>
 		);
 
-		const moreChannelsUI = (
-			<Fragment>
-				<Header>More channels</Header>
-				<Section>
-					{sortedChannels.length > 5 &&
-						sortedChannels.slice(5, 15).map((channel) => (
+		const moreChannelsUI =
+			sortedChannels.length > 5 ? (
+				<Fragment>
+					<Header>More channels</Header>
+
+					<Section>
+						{sortedChannels.slice(5, 15).map((channel) => (
 							<Field>
 								<Mrkdwn raw>{"<#" + channel + ">"}</Mrkdwn>
 							</Field>
 						))}
-				</Section>
-			</Fragment>
-		);
+					</Section>
+				</Fragment>
+			) : undefined;
 
-		const evenMoreChannelsUI = (
-			<Fragment>
-				<Header>Even more channels</Header>
-				<Section>
-					<Mrkdwn raw>
-						{sortedChannels.length > 15 &&
-							sortedChannels
+		const evenMoreChannelsUI =
+			sortedChannels.length > 15 ? (
+				<Fragment>
+					<Header>Even more channels</Header>
+
+					<Section>
+						<Mrkdwn raw>
+							{sortedChannels
 								.slice(15)
 								.map((channel) => "<#" + channel + "> ")}
-					</Mrkdwn>
-				</Section>
-			</Fragment>
-		);
+						</Mrkdwn>
+					</Section>
+				</Fragment>
+			) : undefined;
 
 		/* --------------------------------- Respond -------------------------------- */
 
@@ -189,10 +173,10 @@ const userApp = new App({
 
 		let isUserOwner =
 			(await botApp.client.conversations.info({ channel: channelID }))
-				.channel?.creator === userID;
+				.channel.creator === userID;
 
 		let isUserAdmin = (await botApp.client.users.info({ user: userID }))
-			.user?.is_admin;
+			.user.is_admin;
 
 		/* -------------------------- If not allowed, deny -------------------------- */
 
@@ -222,3 +206,116 @@ const userApp = new App({
 
 	console.log("⚡️ Bolt app is running!");
 })();
+
+/* -------------------------------------------------------------------------- */
+/*                             Get latest messages                            */
+/* -------------------------------------------------------------------------- */
+
+async function getLatestMessages(count: number, respond: RespondFn) {
+	let allMessages;
+
+	/* ----------------------------- Get first page ----------------------------- */
+
+	let currentMessageData = await userApp.client.search.messages({
+		query: "-is:dm",
+		sort: "timestamp",
+		sort_dir: "desc",
+		count: count <= 100 ? count : 100,
+	});
+
+	allMessages = currentMessageData.messages.matches;
+
+	/* ---------------------- Paginate, if there are pages ---------------------- */
+
+	// I hope that one of these days, Slack adds cursor-based
+	// pagination to search.messages :,)
+
+	if (allMessages.length < count) {
+		respond({
+			text: `Due to requesting >100 messages, this may take some time... (expected: ${Math.floor(
+				(count / 100) * 1.25
+			)}-${Math.ceil((count / 100) * 1.75)} seconds)`,
+		});
+	}
+
+	while (allMessages.length < count) {
+		currentMessageData = await userApp.client.search.messages({
+			query: "-is:dm",
+			sort: "timestamp",
+			sort_dir: "desc",
+			count: 100,
+			page: currentMessageData.messages.pagination.page + 1,
+		});
+
+		/* ------------ If <20 more messages are needed, slice the array ------------ */
+
+		if (count - allMessages.length < 20) {
+			allMessages.push(
+				...currentMessageData.messages.matches.slice(
+					0,
+					allMessages.length - count
+				)
+			);
+		} else {
+			allMessages.push(...currentMessageData.messages.matches);
+		}
+
+		/* ---------------------- Sleep, to avoid rate limiting --------------------- */
+
+		await new Promise((resolve) => setTimeout(resolve, 1500));
+	}
+
+	return allMessages;
+}
+
+async function getSortedChannels(messages: Match[]) {
+	let channelCounts = {};
+	let sortedChannels;
+
+	messages.map((match) => {
+		/* ---------------------- Return if not public channel ---------------------- */
+
+		if (
+			match.channel.is_channel !== true ||
+			match.channel.is_private === true
+		)
+			return;
+
+		/* -------------------------- Otherwise, process it ------------------------- */
+
+		let channelID = match.channel.id as string;
+
+		// If channel doesn't exist yet, add the key
+		if (channelCounts[channelID] === undefined)
+			channelCounts[channelID] = 0;
+
+		// Increment the count
+		channelCounts[channelID] += 1;
+	});
+
+	/* ------------------------------ Sort channels ----------------------------- */
+
+	sortedChannels = Object.keys(channelCounts).sort(function (a, b) {
+		return channelCounts[b] - channelCounts[a];
+	});
+
+	/* ----------------------------- Return results ----------------------------- */
+
+	return sortedChannels;
+}
+
+async function getChannelTopics(channels: string[]) {
+	let topics: string[] = [];
+
+	for (const channel of channels) {
+		let topic = (
+			await botApp.client.conversations.info({
+				channel,
+			})
+		).channel.topic.value;
+
+		topics.push(topic);
+	}
+
+	return topics;
+}
